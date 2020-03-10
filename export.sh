41b1b42e8ce4e8d254de4a20a428d3f96ccc51d8
OCM <data@openchargemap.org>
2020-03-10 04:03:17 +0000

    Update

--- export.sh
+++ export.sh
@@ -3,5 +3,6 @@ mongoexport --collection=reference --db=ocm_mirror --out=reference.json
 mongoexport --collection=poi --db=ocm_mirror --out=poi.json
 gzip -f poi.json
 gzip -kf reference.json
+git add *
 git commit -m "Update"
 git push
